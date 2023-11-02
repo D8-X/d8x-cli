85945c86be5f19a33e6ad84b8ad25011047b5ad3
Mantas <j.mantelijo@gmail.com>
2023-11-02 10:23:37 +0200

    add docker service ls/ps info for healthchecks

--- internal/actions/healthchecks.go
+++ internal/actions/healthchecks.go
@@ -4,11 +4,13 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"sort"
 	"strconv"
 	"strings"
 	"time"
 
 	"github.com/D8-X/d8x-cli/internal/configs"
+	"github.com/D8-X/d8x-cli/internal/conn"
 	"github.com/D8-X/d8x-cli/internal/styles"
 	"github.com/charmbracelet/bubbles/spinner"
 	tea "github.com/charmbracelet/bubbletea"
@@ -22,6 +24,16 @@ func (c *Container) HealthCheck(ctx *cli.Context) error {
 	cfg, err := c.ConfigRWriter.Read()
 	if err != nil {
 		return err
+	}
+
+	// Establish manager node ssh connection
+	ip, err := c.HostsCfg.GetMangerPublicIp()
+	if err != nil {
+		return err
+	}
+	managerConn, err := conn.NewSSHConnection(ip, c.DefaultClusterUserName, c.SshKeyPath)
+	if err != nil {
+		return fmt.Errorf("establishing ssh connection to manager node: %w", err)
 	}
 
 	svcsForModel := []*serviceHostnameStatus{}
@@ -49,6 +61,7 @@ func (c *Container) HealthCheck(ctx *cli.Context) error {
 		// Listen for updates from health checker
 		go func(ch chan healthCheckMsg, s *serviceHostnameStatus) {
 			for info := range ch {
+
 				if info.done {
 					s.responseStatus = info.responseStatus
 					s.done = true
@@ -57,15 +70,26 @@ func (c *Container) HealthCheck(ctx *cli.Context) error {
 				}
 				s.currentCtxDeadline = info.nextTimeout
 				s.currentRetry++
+
 			}
 		}(ch, shs)
 	}
-	mdl, err := tea.NewProgram(initHealthCheckModel(svcsForModel)).Run()
+
+	_, err = tea.NewProgram(initHealthCheckModel(healthCheckModel{
+		services: svcsForModel,
+	})).Run()
+
 	if err != nil {
 		return err
 	}
 
-	mdl.(healthCheckModel).allDone()
+	// Once http endpoint checks are done - run docker services check
+	dockerSwarmInfoString, err := healthChecksSwarmServices(managerConn)
+	if err != nil {
+		return fmt.Errorf("retrieving docker swarm info: %w", err)
+	}
+	// Print the docker services info outside the bubbletea program
+	fmt.Printf("\nDocker swarm services status:%s\n", dockerSwarmInfoString)
 
 	return nil
 
@@ -111,6 +135,135 @@ func (c *Container) healthCheckWithBackoff(ch chan healthCheckMsg, svc configs.D
 	return nil
 }
 
+// healthChecksSwarmServices parses services statuses from manager node
+func healthChecksSwarmServices(managerConn conn.SSHConnection) (string, error) {
+
+	cmd := `docker service ls | awk 'NR > 1' | awk  '{print $2}' | xargs docker service ps --format 'table {{.Node}}[##]{{.Name}}[##]{{.CurrentState}}[##]{{.Error}}[##]' --no-trunc`
+
+	psOutput, err := managerConn.ExecCommand(cmd)
+	if err != nil {
+		return "", err
+	}
+
+	lsOutput, err := managerConn.ExecCommand("docker service ls")
+	if err != nil {
+		return "", err
+	}
+
+	psLines := strings.Split(string(psOutput), "\n")[1:]
+	lsLines := strings.Split(string(lsOutput), "\n")[1:]
+
+	// docker ps output info
+	type svcPsInfo struct {
+		node         string
+		currentState string
+		err          string
+		// svc task name (appened with .<int>)
+		name string
+	}
+
+	type svcInfo struct {
+		name string
+		// Number of running replicas
+		running int
+		// Number of total replicas defined to run
+		total          int
+		replicasString string
+		psInfo         []svcPsInfo
+	}
+
+	// Parse `docker ls` info
+	//Fields: ID,NAME,MODE,REPLICAS,IMAGE,PORTS
+	svcNames := []string{}
+	svcs := map[string]svcInfo{}
+	for _, line := range lsLines {
+		fields := strings.Fields(line)
+
+		if len(fields) >= 3 {
+			replicas := strings.Split(fields[3], "/")
+			running, _ := strconv.Atoi(replicas[0])
+			total, _ := strconv.Atoi(replicas[1])
+			info := svcInfo{
+				name:           fields[1],
+				running:        running,
+				total:          total,
+				psInfo:         []svcPsInfo{},
+				replicasString: fields[3],
+			}
+			svcs[fields[1]] = info
+			svcNames = append(svcNames, fields[1])
+		}
+	}
+	sort.StringSlice(svcNames).Sort()
+
+	// Parse `docker ps` info
+	// NODE, NAME, CURRENT STATE, ERROR
+	for _, line := range psLines {
+		// See the cmd for separator
+		fields := strings.Split(line, "[##]")
+		for i, f := range fields {
+			fields[i] = strings.TrimSpace(f)
+		}
+
+		if len(fields) >= 3 {
+			name := strings.Split(fields[1], ".")[0]
+			err := fields[3]
+
+			if v, ok := svcs[name]; ok {
+				v.psInfo = append(v.psInfo, svcPsInfo{
+					node:         fields[0],
+					currentState: fields[2],
+					err:          err,
+					name:         fields[1],
+				})
+				svcs[name] = v
+			}
+		}
+	}
+
+	// Build the output
+	fullOutput := strings.Builder{}
+	for _, svcName := range svcNames {
+		out := strings.Builder{}
+		v := svcs[svcName]
+		out.WriteByte('\n')
+
+		// Name and instances
+		nameAndInstances := fmt.Sprintf(
+			"%s\n  instances: %s",
+			svcName, v.replicasString,
+		)
+		out.WriteString(nameAndInstances)
+
+		// Instances info
+
+		for _, psInfo := range v.psInfo {
+			out.WriteString("\n  \\_ ")
+			out.WriteString(psInfo.name)
+			out.WriteString(" on ")
+			out.WriteString(psInfo.node)
+			out.WriteString(" status ")
+			out.WriteString(psInfo.currentState)
+
+			if psInfo.err != "" {
+				out.WriteString(" ")
+				out.WriteString(psInfo.err)
+			}
+		}
+
+		if v.running < v.total {
+			fullOutput.WriteString(styles.ErrorText.Render(
+				out.String(),
+			))
+		} else {
+			fullOutput.WriteString(out.String())
+		}
+
+	}
+
+	return fullOutput.String(), nil
+}
+
 const (
 	notok   = "❌"
 	ok      = "✅"
@@ -134,14 +287,12 @@ type healthCheckModel struct {
 	spinner  spinner.Model
 }
 
-func initHealthCheckModel(svcs []*serviceHostnameStatus) healthCheckModel {
+func initHealthCheckModel(h healthCheckModel) healthCheckModel {
 	s := spinner.New()
 	s.Spinner = spinner.Dot
 	s.Style = lipgloss.NewStyle().Foreground(styles.D8XPurple)
-	return healthCheckModel{
-		spinner:  s,
-		services: svcs,
-	}
+	h.spinner = s
+	return h
 }
 
 func (h healthCheckModel) Init() tea.Cmd {
@@ -179,7 +330,7 @@ func (m healthCheckModel) allDone() bool {
 }
 
 func (h healthCheckModel) View() string {
-	b := strings.Builder{}
+	httpHealthChecks := strings.Builder{}
 
 	for _, svc := range h.services {
 		spinner := ""
@@ -205,6 +356,7 @@ func (h healthCheckModel) View() string {
 				responseStatus = styles.ErrorText.Render(responseStatus)
 				spinner = warning
 			}
+			spinner += " "
 
 		} else {
 			spinner = h.spinner.View()
@@ -218,7 +370,7 @@ func (h healthCheckModel) View() string {
 			retry = "(" + retry + ")"
 		}
 
-		b.WriteString(
+		httpHealthChecks.WriteString(
 			fmt.Sprintf(
 				"%s %s %s %s %s %s %s",
 				spinner,
@@ -231,13 +383,15 @@ func (h healthCheckModel) View() string {
 			),
 		)
 
-		b.WriteByte('\n')
+		httpHealthChecks.WriteByte('\n')
 	}
 
 	title := "Performing health checks"
+	dockerSwarmInfo := "\n" + h.spinner.View() + "Loading Docker Swarm Services info\n"
 	if h.allDone() {
 		title = "Health checks done"
+		dockerSwarmInfo = ""
 	}
 
-	return title + "\n\n" + b.String()
+	return title + "\n\nHTTP Endpoints:\n" + httpHealthChecks.String() + dockerSwarmInfo
 }
