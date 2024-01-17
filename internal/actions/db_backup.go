0b8f3ca15f8470a1f281ac872f9fcd53ddbfd1c7
Mantas <j.mantelijo@gmail.com>
2024-01-17 20:39:15 +0200

    pg_dump latest version, db backups readme

--- internal/actions/db_backup.go
+++ internal/actions/db_backup.go
@@ -1,18 +1,26 @@
 package actions
 
 import (
+	"context"
 	"fmt"
 	"io"
+	"net"
 	"os"
 	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/D8-X/d8x-cli/internal/conn"
 	"github.com/D8-X/d8x-cli/internal/styles"
-	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5"
 	"github.com/pkg/sftp"
 	"github.com/urfave/cli/v2"
 )
+
+// Latest available stable postgresql client version to use.
+const CurrentMaximumPostgresVersion = 16
 
 // BackupDb performs database backup
 func (c *Container) BackupDb(ctx *cli.Context) error {
@@ -33,7 +41,7 @@ func (c *Container) BackupDb(ctx *cli.Context) error {
 	}
 
 	// Parse the database dsn string
-	pgCfg, err := pgconn.ParseConfig(cfg.DatabaseDSN)
+	pgCfg, err := pgx.ParseConfig(cfg.DatabaseDSN)
 	if err != nil {
 		return fmt.Errorf("parsing database connection string: %w", err)
 	}
@@ -49,15 +57,55 @@ func (c *Container) BackupDb(ctx *cli.Context) error {
 		return err
 	}
 
+	// Retrieve the postgres version on target database
+	fmt.Printf("Determining postgres version\n")
+	pgConn, err := pgConnTunnel(managerConn, pgCfg)
+	if err != nil {
+		return err
+	}
+	row := pgConn.QueryRow(context.Background(), "select version()")
+	versionString := ""
+	if err := row.Scan(&versionString); err != nil {
+		return fmt.Errorf("retrieving postgres connection version: %w", err)
+	}
+	re := regexp.MustCompile(`PostgreSQL ([0-9]+)\.?([0-9]+).*`)
+	versionMatches := re.FindStringSubmatch(versionString)
+	if len(versionMatches) >= 3 {
+		majorVersion := versionMatches[1]
+		minorVersion := versionMatches[2]
+		versionString = majorVersion + "." + minorVersion
+
+	}
+	fmt.Printf("Postgres server at %s version: %s\n", pgCfg.Host, versionString)
+
+	// We default to maximum postgresq-client-x version available since it is
+	// backwards compatible.
+	cmd := "apt-cache search --names-only ^postgresql-client-* | awk '{print $1}'"
+	pgClientPackages, err := managerConn.ExecCommand(cmd)
+	maxPgVersion := CurrentMaximumPostgresVersion
+	if err != nil {
+		for _, pkgName := range strings.Split(string(pgClientPackages), "\n") {
+			versionStr := strings.TrimPrefix(
+				strings.TrimSpace(pkgName),
+				"postgresql-client-",
+			)
+			// Parse only whole int versions
+			if version, err := strconv.ParseInt(versionStr, 10, 64); err == nil && maxPgVersion < int(version) {
+				maxPgVersion = int(version)
+			}
+		}
+	}
+	aptPgClientPackage := "postgresql-client-" + strconv.Itoa(maxPgVersion)
+
 	// Make sure pg dump (postgres 15) is installed on the manager. Let's use
 	// the public postgres apt repo and set it up. It contains all latest
 	// versions of postgres
-	fmt.Println("Ensuring pg_dump is installed on manager server")
+	fmt.Printf("Ensuring pg_dump is installed on manager server (%s)\n", aptPgClientPackage)
 	installScriptSteps := []string{
 		`echo "deb https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list`,
 		"wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -",
 		"apt-get update -y",
-		"apt-get -y install postgresql-client-15",
+		"apt-get -y install " + aptPgClientPackage,
 	}
 	for _, s := range installScriptSteps {
 		cmd := fmt.Sprintf(`echo "%s" | sudo -S bash -c '%s'`, pwd, s)
@@ -134,4 +182,12 @@ func (c *Container) BackupDb(ctx *cli.Context) error {
 	}
 
 	return nil
+}
+
+func pgConnTunnel(manager conn.SSHConnection, pgCfg *pgx.ConnConfig) (*pgx.Conn, error) {
+	pgCfg.DialFunc = func(ctx context.Context, network, addr string) (net.Conn, error) {
+		return manager.GetClient().DialContext(ctx, network, addr)
+	}
+
+	return pgx.ConnectConfig(context.Background(), pgCfg)
 }
